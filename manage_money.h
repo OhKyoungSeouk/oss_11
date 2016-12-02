e205baef34d73aacd2552b916b35945ad6000034
czd0m <czd0m@naver.com>
2016-12-02 10:16:02 +0900

    Update manage_money.h

--- manage_money.h
+++ manage_money.h
@@ -22,13 +22,13 @@ void depositMoney(struct Account *tmp)
 	printf("계좌에 금액을 더합니다.\n");
 	//printf("계좌번호를 입력하시오\n");
 	//scanf(" %d", &id);
-	printf("입금할 금액을 입력하시오\n");
+	printf("입금할 금액을 입력하시오\n");fflush(stdout);
 	scanf(" %ld", &money);
 	fflush(stdin);
 
 	while(money > 0)
 	{
-		printf("입력한 계좌번호와 금액이 각각 %05ld, %ld가 맞습니까?(y/n)\n", tmp->account_number, money);
+		printf("입력한 계좌번호와 금액이 각각 %05ld, %ld가 맞습니까?(y/n)\n", tmp->account_number, money);fflush(stdout);
 		scanf(" %c", &q);
 		if (q == 'y') {
 			tmp->balance += money;
@@ -36,7 +36,7 @@ void depositMoney(struct Account *tmp)
 		}
 		else
 		{
-			printf("잘못 입력하였습니다. 다시 입력하시오.(y/n)\n");
+			printf("잘못 입력하였습니다. 다시 입력하시오.(y/n)\n");fflush(stdout);
 			scanf(" %c", &q);
 			if(q == 'n')
 				break;
@@ -53,12 +53,12 @@ void withdraw(struct Account *tmp)
 	printf("계좌에서 출금을 합니다.\n");
 	//printf("계좌번호를 입력하시오\n");
 	//scanf(" %d", &id);
-	printf("출금할 금액을 입력하시오\n");
+	printf("출금할 금액을 입력하시오\n");fflush(stdout);
 	scanf(" %ld", &money);
 	fflush(stdin);
 
 	while(money > 0) {
-		printf("입력한 계좌번호와 금액이 각각 %05ld, %ld가 맞습니까?(Y/N)\n", tmp->account_number, money);
+		printf("입력한 계좌번호와 금액이 각각 %05ld, %ld가 맞습니까?(Y/N)\n", tmp->account_number, money);fflush(stdout);
 		scanf(" %c", &q);
 		if (q == 'y') {
 			tmp->balance -= money;
